531bf77e4f0c36ec8106e58ea483ea9fb5226950
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-28 16:31:37 +0200

    [add]:carousel btn with conditions reset counter

--- js/script.js
+++ js/script.js
@@ -32,135 +32,30 @@ createApp ({
           title: "Marvel's Avengers",
           text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.'
         }
-      ]
-
-      
+      ]  
     }
+
+  },  
+
+  methods:{
+    upDown(isNext){
+      isNext ? this.counter++ : this.counter--;
+      if(this.counter < 0){
+        this.counter = this.carousel.length -1;
+      }else if (this.counter === this.carousel.length ){
+        this.counter = 0;
+      }
+    }  
   }
+
+
+    
+ 
 }).mount('#app');
 
 
 
 
-
-
-
-// NEW CAROUSEL WITH OBJECTS AND forEach
-// images.forEach((element) =>{
-//   itemsCarousel.innerHTML += `
-//   <div class = 'block-carousel newItem hide'>
-//     <img src="${element.image}">
-//     <div class = 'block-description'>
-//       <h3>${element.title}</h3>
-//       <p>${element.text}</p>
-//     </div>
-//   </div>`;
-//   thumbnails.innerHTML += `<img src="${element.image}"class= "thumbItem inactive">`;
-// })
-
-// const newItem = document.getElementsByClassName('newItem');
-
-// const thumbItem = document.getElementsByClassName('thumbItem')
-
-
-// prendi indice 0 del nuovo array
-// newItem[0].classList.remove('hide');
-// thumbItem[0].classList.add('active');
-
-
-// // bottone down
-// btnDown.addEventListener('click',function(){
-
-//   addRemove();
-
-//   counter++;
-  
-//   if (counter === newItem.length) counter = 0;
-
-//   removeAdd();
-// })
-
-
-// // Auto carousel down btn
-// btnDown.addEventListener('click',startDown)
-
-//   function startDown(){
-//     this.removeEventListener('click',startDown)
-//     validCarousel = false;
-    
-    
-//     const clock = setInterval(function() {
-//         if(!validCarousel){
-  
-//         addRemove();
-      
-//         counter++;
-        
-//         if (counter === newItem.length) counter = 0;
-      
-//         removeAdd();
-
-//         btnUp.addEventListener('click',startUp);
-
-//       }else {
-//         clearInterval(clock);
-//       }   
-//       }, 2000);
-//   }
-
-// // bottone  su
-// btnUp.addEventListener('click',function(){
-    
-//   addRemove();
-
-//   counter--;
-
-//   if(counter < 0) counter = newItem.length -1;
-
-//   removeAdd();
-// })
-
-// // Auto carousel up btn
-// btnUp.addEventListener('click',startUp)
-
-//   function startUp(){
-//     this.removeEventListener('click',startUp)
-//     validCarousel = true;
-    
-    
-//     const clock = setInterval(function() {
-//         if(validCarousel){
-  
-//         addRemove();
-      
-//         counter--;
-        
-//         if(counter < 0) counter = newItem.length -1;
-      
-//         removeAdd();
-        
-//         btnDown.addEventListener('click',startDown);
-//       }else {
-//         clearInterval(clock);
-        
-//       }   
-//       }, 2000);
-//   }
-
-
-
-// // function add/remove
-// function addRemove(){
-//   newItem[counter].classList.add('hide');
-//   thumbItem[counter].classList.remove('active');
-// }
-
-
-// // function remove/add
-// function removeAdd(){
-//   newItem[counter].classList.remove('hide');
-//   thumbItem[counter].classList.add('active');
-// }
 
  
 
