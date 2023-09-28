bc95800ed9c7219682e2b7f394ff61ce1b0d9494
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-28 14:59:42 +0200

    [add]: some changes and comments

--- js/script.js
+++ js/script.js
@@ -36,7 +36,8 @@ createApp ({
       
     }
   }
-}).mount('#app')
+}).mount('#app');
+
 
 
 
@@ -44,121 +45,121 @@ createApp ({
 
 
 // NEW CAROUSEL WITH OBJECTS AND forEach
-images.forEach((element) =>{
-  itemsCarousel.innerHTML += `
-  <div class = 'block-carousel newItem hide'>
-    <img src="${element.image}">
-    <div class = 'block-description'>
-      <h3>${element.title}</h3>
-      <p>${element.text}</p>
-    </div>
-  </div>`;
-  thumbnails.innerHTML += `<img src="${element.image}"class= "thumbItem inactive">`;
-})
+// images.forEach((element) =>{
+//   itemsCarousel.innerHTML += `
+//   <div class = 'block-carousel newItem hide'>
+//     <img src="${element.image}">
+//     <div class = 'block-description'>
+//       <h3>${element.title}</h3>
+//       <p>${element.text}</p>
+//     </div>
+//   </div>`;
+//   thumbnails.innerHTML += `<img src="${element.image}"class= "thumbItem inactive">`;
+// })
 
-const newItem = document.getElementsByClassName('newItem');
+// const newItem = document.getElementsByClassName('newItem');
 
-const thumbItem = document.getElementsByClassName('thumbItem')
+// const thumbItem = document.getElementsByClassName('thumbItem')
 
 
 // prendi indice 0 del nuovo array
-newItem[0].classList.remove('hide');
-thumbItem[0].classList.add('active');
+// newItem[0].classList.remove('hide');
+// thumbItem[0].classList.add('active');
 
 
-// bottone down
-btnDown.addEventListener('click',function(){
+// // bottone down
+// btnDown.addEventListener('click',function(){
 
-  addRemove();
+//   addRemove();
 
-  counter++;
+//   counter++;
   
-  if (counter === newItem.length) counter = 0;
+//   if (counter === newItem.length) counter = 0;
 
-  removeAdd();
-})
+//   removeAdd();
+// })
 
 
-// Auto carousel down btn
-btnDown.addEventListener('click',startDown)
+// // Auto carousel down btn
+// btnDown.addEventListener('click',startDown)
 
-  function startDown(){
-    this.removeEventListener('click',startDown)
-    validCarousel = false;
+//   function startDown(){
+//     this.removeEventListener('click',startDown)
+//     validCarousel = false;
     
     
-    const clock = setInterval(function() {
-        if(!validCarousel){
+//     const clock = setInterval(function() {
+//         if(!validCarousel){
   
-        addRemove();
+//         addRemove();
       
-        counter++;
+//         counter++;
         
-        if (counter === newItem.length) counter = 0;
+//         if (counter === newItem.length) counter = 0;
       
-        removeAdd();
+//         removeAdd();
 
-        btnUp.addEventListener('click',startUp);
+//         btnUp.addEventListener('click',startUp);
 
-      }else {
-        clearInterval(clock);
-      }   
-      }, 2000);
-  }
+//       }else {
+//         clearInterval(clock);
+//       }   
+//       }, 2000);
+//   }
 
-// bottone  su
-btnUp.addEventListener('click',function(){
+// // bottone  su
+// btnUp.addEventListener('click',function(){
     
-  addRemove();
+//   addRemove();
 
-  counter--;
+//   counter--;
 
-  if(counter < 0) counter = newItem.length -1;
+//   if(counter < 0) counter = newItem.length -1;
 
-  removeAdd();
-})
+//   removeAdd();
+// })
 
-// Auto carousel up btn
-btnUp.addEventListener('click',startUp)
+// // Auto carousel up btn
+// btnUp.addEventListener('click',startUp)
 
-  function startUp(){
-    this.removeEventListener('click',startUp)
-    validCarousel = true;
+//   function startUp(){
+//     this.removeEventListener('click',startUp)
+//     validCarousel = true;
     
     
-    const clock = setInterval(function() {
-        if(validCarousel){
+//     const clock = setInterval(function() {
+//         if(validCarousel){
   
-        addRemove();
+//         addRemove();
       
-        counter--;
+//         counter--;
         
-        if(counter < 0) counter = newItem.length -1;
+//         if(counter < 0) counter = newItem.length -1;
       
-        removeAdd();
+//         removeAdd();
         
-        btnDown.addEventListener('click',startDown);
-      }else {
-        clearInterval(clock);
+//         btnDown.addEventListener('click',startDown);
+//       }else {
+//         clearInterval(clock);
         
-      }   
-      }, 2000);
-  }
+//       }   
+//       }, 2000);
+//   }
 
 
 
-// function add/remove
-function addRemove(){
-  newItem[counter].classList.add('hide');
-  thumbItem[counter].classList.remove('active');
-}
+// // function add/remove
+// function addRemove(){
+//   newItem[counter].classList.add('hide');
+//   thumbItem[counter].classList.remove('active');
+// }
 
 
-// function remove/add
-function removeAdd(){
-  newItem[counter].classList.remove('hide');
-  thumbItem[counter].classList.add('active');
-}
+// // function remove/add
+// function removeAdd(){
+//   newItem[counter].classList.remove('hide');
+//   thumbItem[counter].classList.add('active');
+// }
 
  
 
